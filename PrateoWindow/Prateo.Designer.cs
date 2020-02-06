79767e8f44cdce9514cc1a09f2f2403778dfd5e4
David Meyers <David.Meyers@newmont.com>
2020-02-06 08:26:26 -0700

    Added ability to draw axis and tick marks

--- PrateoWindow/Prateo.Designer.cs
+++ PrateoWindow/Prateo.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button_Build = new System.Windows.Forms.Button();
+            this.comboBoxConnectedServers = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPiPoint = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxStartTime = new System.Windows.Forms.TextBox();
+            this.textBoxEndTime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +52,7 @@
             // 
             // button_Build
             // 
-            this.button_Build.Location = new System.Drawing.Point(12, 92);
+            this.button_Build.Location = new System.Drawing.Point(12, 282);
             this.button_Build.Name = "button_Build";
             this.button_Build.Size = new System.Drawing.Size(75, 23);
             this.button_Build.TabIndex = 1;
@@ -52,17 +60,96 @@
             this.button_Build.UseVisualStyleBackColor = true;
             this.button_Build.Click += new System.EventHandler(this.button_Build_Click);
             // 
+            // comboBoxConnectedServers
+            // 
+            this.comboBoxConnectedServers.FormattingEnabled = true;
+            this.comboBoxConnectedServers.Location = new System.Drawing.Point(12, 67);
+            this.comboBoxConnectedServers.Name = "comboBoxConnectedServers";
+            this.comboBoxConnectedServers.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxConnectedServers.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selected PI Server:";
+            // 
+            // textBoxPiPoint
+            // 
+            this.textBoxPiPoint.Location = new System.Drawing.Point(12, 118);
+            this.textBoxPiPoint.Name = "textBoxPiPoint";
+            this.textBoxPiPoint.Size = new System.Drawing.Size(148, 20);
+            this.textBoxPiPoint.TabIndex = 4;
+            this.textBoxPiPoint.Text = "N_MML12001LastTrip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter PI Point:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Start Time:";
+            // 
+            // textBoxStartTime
+            // 
+            this.textBoxStartTime.Location = new System.Drawing.Point(12, 187);
+            this.textBoxStartTime.Name = "textBoxStartTime";
+            this.textBoxStartTime.Size = new System.Drawing.Size(148, 20);
+            this.textBoxStartTime.TabIndex = 7;
+            this.textBoxStartTime.Text = "*-7d";
+            // 
+            // textBoxEndTime
+            // 
+            this.textBoxEndTime.Location = new System.Drawing.Point(12, 235);
+            this.textBoxEndTime.Name = "textBoxEndTime";
+            this.textBoxEndTime.Size = new System.Drawing.Size(148, 20);
+            this.textBoxEndTime.TabIndex = 9;
+            this.textBoxEndTime.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "End Time:";
+            // 
             // Prateo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 450);
+            this.Controls.Add(this.textBoxEndTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxStartTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPiPoint);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxConnectedServers);
             this.Controls.Add(this.button_Build);
             this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
             this.Name = "Prateo";
             this.Text = "Prateo";
+            this.Load += new System.EventHandler(this.Prateo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +157,14 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button_Build;
+        private System.Windows.Forms.ComboBox comboBoxConnectedServers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPiPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxStartTime;
+        private System.Windows.Forms.TextBox textBoxEndTime;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- PrateoWindow/Prateo.cs
+++ PrateoWindow/Prateo.cs
@@ -19,15 +19,78 @@ namespace PrateoWindow
 
         private void button_Build_Click(object sender, EventArgs e)
         {
+            pictureBox1.Image = null;
             Create_FrequencyChart();
         }
 
-        private void Draw_Rectangles(List<RectangleF> rectangleList)
+        
+        private void Create_FrequencyChart()
         {
-            Bitmap bitmap = (Bitmap)pictureBox1.Image;
-            SolidBrush brush = new SolidBrush(Color.Blue);
+            //MyPI.AFserverName = "APBDPAP0043";
+            //MyPI.AFdatabaseName = "2004_NBG";
+            MyPI.PIservername = comboBoxConnectedServers.SelectedItem.ToString();
+            string pipointName = textBoxPiPoint.Text;
+            string startTime = textBoxStartTime.Text;
+            string endTime = textBoxEndTime.Text;
+            
+            GetRecordedValues getRecordedValues = new GetRecordedValues(startTime, endTime, pipointName, MyPI.PIservername);
+            if(getRecordedValues.ValueList != null)
+            {
+                var valuelist = new SanitizeAFValues(getRecordedValues.ValueList).afValues;
 
-            using (Graphics g = pictureBox1.CreateGraphics())
+                List<Tuple<string, int>> tupleList = new GetTupleList(valuelist).tupleList;
+
+                var maximum_y = tupleList.Max(x => x.Item2);
+                var barCount = tupleList.Count;
+
+                int padding_x = 50;
+                int padding_y = 50;
+
+                int picturebox_x = pictureBox1.Width;
+                int picturebox_y = pictureBox1.Height;
+
+                int window_x = picturebox_x - padding_x * 2;
+                int window_y = picturebox_y - padding_y * 2;
+
+                float bar_x_ratio = (float)0.80;
+                float bar_x = window_x / barCount * bar_x_ratio;
+
+                float barspace_x = (1 - bar_x_ratio) * (window_x / barCount);
+
+                float space_x = padding_x + barspace_x / 2;
+                float start_x = space_x;
+
+                List<RectangleF> rectangleList = new List<RectangleF>();
+
+                for (int i = 0; i < barCount; i++)
+                {
+                    int value_y = tupleList[i].Item2;
+                    float rectangle_y = value_y * window_y / maximum_y;
+                    RectangleF rectangle = new RectangleF(start_x, padding_y + (window_y - rectangle_y), bar_x, rectangle_y);
+                    rectangleList.Add(rectangle);
+                    start_x += bar_x + barspace_x;
+                }
+
+                Bitmap bitmapWithRectangle = Draw_Rectangles(rectangleList);
+                Bitmap bitmapWithAxis = Draw_Axis(bitmapWithRectangle, padding_x, padding_y);
+                Bitmap bitmapWithTicks = Draw_TickMarks(bitmapWithAxis, padding_x, padding_y, rectangleList);
+                pictureBox1.Image = bitmapWithTicks;
+            }
+            else
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("PI Point not found on the selected server", "Error");
+                }
+            }
+        }
+
+        private Bitmap Draw_Rectangles(List<RectangleF> rectangleList)
+        {
+            SolidBrush brush = new SolidBrush(Color.Blue);
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            using (Graphics g = Graphics.FromImage(bitmap))
             {
                 foreach (RectangleF rectangle in rectangleList)
                 {
@@ -36,53 +99,55 @@ namespace PrateoWindow
                 }
 
             }
+            return bitmap;
+
         }
 
-        private void Create_FrequencyChart()
+        private Bitmap Draw_Axis(Bitmap bitmap, int padding_x, int padding_y)
         {
-            MyPI.AFserverName = "APBDPAP0043";
-            MyPI.AFdatabaseName = "2004_NBG";
-            MyPI.PIservername = "AFAHPAP0010";
-            string pipointName = "N_MML12001LastTrip";
-
-            GetRecordedValues getRecordedValues = new GetRecordedValues("*-7d", "*", pipointName, MyPI.PIservername);
-            var valuelist = new SanitizeAFValues(getRecordedValues.ValueList).afValues;
-
-            List<Tuple<string, int>> tupleList = new GetTupleList(valuelist).tupleList;
-
-            var maximum_y = tupleList.Max(x => x.Item2);
-            var barCount = tupleList.Count;
-
-            int padding_x = 25;
-            int padding_y = 25;
-
-            int picturebox_x = pictureBox1.Width;
-            int picturebox_y = pictureBox1.Height;
-
-            int window_x = picturebox_x - padding_x * 2;
-            int window_y = picturebox_y - padding_y * 2;
-
-            float bar_x_ratio = (float)0.80;
-            float bar_x = window_x / barCount * bar_x_ratio;
-
-            float barspace_x = (1 - bar_x_ratio) * (window_x / barCount);
-
-            float space_x = padding_x + barspace_x / 2;
-            float start_x = space_x;
-
-            List<RectangleF> rectangleList = new List<RectangleF>();
-
-            for (int i = 0; i < barCount; i++)
+            Pen blackPen = new Pen(Color.Black);
+            using (Graphics g = Graphics.FromImage(bitmap))
             {
-                int value_y = tupleList[i].Item2;
-                float rectangle_y = value_y * window_y / maximum_y;
-                RectangleF rectangle = new RectangleF(start_x, padding_y + (window_y - rectangle_y), bar_x, rectangle_y);
-                rectangleList.Add(rectangle);
-                start_x += bar_x + barspace_x;
+                g.DrawLine(blackPen, (float)padding_x, (float)padding_y, (float)padding_x, (float)(pictureBox1.Height - padding_y));
+                g.DrawLine(blackPen, (float)padding_x, (float)(pictureBox1.Height - padding_y), (float)(pictureBox1.Width - padding_x), (float)(pictureBox1.Height - padding_y));
             }
+            return bitmap;
+        }
 
-            Draw_Rectangles(rectangleList);
+        private Bitmap Draw_TickMarks(Bitmap bitmap, int padding_x, int padding_y, List<RectangleF> rectangleList)
+        {
+            Pen blackPen = new Pen(Color.Black);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                foreach(RectangleF rectangle in rectangleList)
+                {
+                    float tick_x = rectangle.X + (rectangle.Width / 2);
+                    float tick_y = (float)(pictureBox1.Height - padding_y);
+                    g.DrawLine(blackPen, tick_x, tick_y, tick_x, tick_y + 5);
+                }
 
+            }
+            return bitmap;
+        }
+
+
+
+        private void Fill_ComboBox()
+        {
+            GetConnectedServers getConnectedServers = new GetConnectedServers();
+            var connectedServers = getConnectedServers.piServers;
+            object[] itemObject = new object[connectedServers.Count];
+            for(int i =0; i<connectedServers.Count;i++)
+            {
+                itemObject[i] = connectedServers[i].Name;
+            }
+            comboBoxConnectedServers.Items.AddRange(itemObject);
+            comboBoxConnectedServers.SelectedItem = itemObject[0];
+        }
+
+        private void Prateo_Load(object sender, EventArgs e)
+        {
+            Fill_ComboBox();
         }
     }
 }
